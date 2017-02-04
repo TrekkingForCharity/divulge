588b278e6f4e81bf83b680424a5bacb9bf39e367
Andrew Davis <andrew@deviouscreation.com>
2017-02-04 23:53:56 +0000

    Moving build to cake

--- Divulge/AssemblyInfo.cs
+++ Divulge/AssemblyInfo.cs
@@ -1,6 +1,8 @@
-﻿using System.Reflection;
+using System.Reflection;
 
 [assembly: AssemblyTitle("Divulge")]
 [assembly: AssemblyProduct("Divulge")]
-[assembly: AssemblyVersion("0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+
+[assembly: AssemblyInformationalVersion("0.1.0+2.Branch.master.Sha.9001e4a1be437a8734456b3fee9fe191d52a66c4")]
